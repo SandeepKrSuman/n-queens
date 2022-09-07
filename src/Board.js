6e04a9e66d2fcf18a09df4a542a5452dbd19deca
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2022-09-07 17:17:45 +0530

    auto create board with input n

--- src/Board.js
+++ src/Board.js
@@ -1,109 +1,40 @@
-import React from "react";
 import "./Board.css";
 
-export default function Board() {
+export default function ChessBoard() {
+  const n = 8;
   return (
     <table className="chess-board">
       <tbody>
-        <tr>
-          <th>8</th>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-        </tr>
-        <tr>
-          <th>7</th>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-        </tr>
-        <tr>
-          <th>6</th>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-        </tr>
-        <tr>
-          <th>5</th>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-        </tr>
-        <tr>
-          <th>4</th>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-        </tr>
-        <tr>
-          <th>3</th>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-        </tr>
-        <tr>
-          <th>2</th>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-        </tr>
-        <tr>
-          <th>1</th>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-          <td className="dark"></td>
-          <td className="light"></td>
-        </tr>
-        <tr>
-          <th></th>
-          <th>a</th>
-          <th>b</th>
-          <th>c</th>
-          <th>d</th>
-          <th>e</th>
-          <th>f</th>
-          <th>g</th>
-          <th>h</th>
-        </tr>
+        {Array(n)
+          .fill(null)
+          .map((value, index) => (
+            <tr key={index}>
+              {Array(n)
+                .fill(null)
+                .map((val, ind) => (
+                  <td
+                    className={
+                      n % 2 === 0
+                        ? index % 2 === 0
+                          ? ind % 2 === 0
+                            ? "light"
+                            : "dark"
+                          : ind % 2 === 0
+                          ? "dark"
+                          : "light"
+                        : index % 2 === 0
+                        ? ind % 2 === 0
+                          ? "dark"
+                          : "light"
+                        : ind % 2 === 0
+                        ? "light"
+                        : "dark"
+                    }
+                    key={ind}
+                  />
+                ))}
+            </tr>
+          ))}
       </tbody>
     </table>
   );
